0d49810f4fa3ef7f68c61ad743f12d3509993a1e
TDSon30 <tdson.caa@gmail.com>
2025-06-05 09:14:44 +0700

    Update configure_training.py

--- configure_training.py
+++ configure_training.py
@@ -7,25 +7,28 @@ import yaml
 import os
 
 def create_data_yaml(path_to_classes_txt, path_to_data_yaml):
- # Read class.txt to get class names
+    # Read class.txt to get class names
     if not os.path.exists(path_to_classes_txt):
         print(f'classes.txt file not found! Please create a classes.txt labelmap and move it to {path_to_classes_txt}')
         return
+
+    classes = []
     with open(path_to_classes_txt, 'r') as f:
-        classes = []
-    for line in f.readlines():
-      if len(line.strip()) == 0: continue
-      classes.append(line.strip())
+        for line in f.readlines():
+            if len(line.strip()) == 0:
+                continue
+            classes.append(line.strip())
+
     number_of_classes = len(classes)
 
-  # Create data dictionary
+    # Create data dictionary
     data = {
         'path': '/content/YOLO/data',
         'train': 'train/images',
         'val': 'validation/images',
         'nc': number_of_classes,
         'names': classes
-    } 
+    }
 
     # Write data dictionary to data.yaml
     with open(path_to_data_yaml, 'w') as f:
@@ -33,6 +36,7 @@ def create_data_yaml(path_to_classes_txt, path_to_data_yaml):
 
     print(f'data.yaml file created successfully at {path_to_data_yaml}')
     return
+
 
 # Define path to classes.txt and run function
 path_to_classes_txt = '/content/YOLO/custom_data/classes.txt'
